90ab84114c693526449942b31e02ccd0539b988c
James Liang <james.liang.cje@icloud.com>
2021-03-20 15:39:49 -0400

    updated

--- ddos-git.sh
+++ ddos-git.sh
@@ -2,7 +2,7 @@
 for i in {1..10}
 do 
 echo $i >> test.txt
-#git stash save && git push --force origin "stash@{0}:master" && git stash pop
+git stash save && git push --force origin "stash@{0}:master" && git stash pop
 
 done
 
